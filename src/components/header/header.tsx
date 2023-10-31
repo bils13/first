fd6cc56fe80738924a61caa63beb4a50c91ab35d
Gabriel Souza <biel.als@icloud.com>
2023-10-31 20:54:22 -0300

    feat: Estrutura inicial

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import styled from "styled-components"
+
+const HeaderContainer = styled.header`
+
+`
+
+export function Header(){
+    return(
+        <HeaderContainer>
+            
+        </HeaderContainer>
+    )
+}
---
+++ src/utils/fonts/fonts.ts
@@ -0,0 +1,9 @@
+import gothamBlackFont from 'next/font/local'
+import gothamBoldFont from 'next/font/local'
+import gothamBookFont from 'next/font/local'
+import gothamLightFont from 'next/font/local'
+
+export const gothamBlack = gothamBlackFont({ src: '../../node_modules/next/font/local/gotham-black.woff2'})
+export const gothamBold = gothamBoldFont({ src: '../../node_modules/next/font/local/gotham-bold.woff2'})
+export const gothamBook = gothamBookFont({ src: '../../node_modules/next/font/local/gotham-book.woff2'})
+export const gothamLight = gothamLightFont({ src: '../../node_modules/next/font/local/gotham-light.woff2'})
